1cbf84022f5264a529a0b17d9add31d420e34633
Jason Olano <44100981+olano002@users.noreply.github.com>
2018-11-06 18:38:07 -0800

    Update SignUpPage.xaml.cs

--- Proto1/Proto1/SignUpPage.xaml.cs
+++ Proto1/Proto1/SignUpPage.xaml.cs
@@ -16,5 +16,34 @@ namespace Proto1
 		{
 			InitializeComponent ();
 		}
+		
+		async void OnSignUpButtonClicked (object sender, EventArgs e)
+		{
+			var user = new User(){
+				FirstName = firstnameEntry.Text,
+				LastName = lastnameEntry.Text,
+				Username = usernameEntry.Text,
+				Password = passwordEntry.Text,
+				Email = emailEntry.Text
+			};
+			
+		var signUpSucceeded = AreDetailsValid (user);
+			if (signUpSucceeded) {
+				var rootPage = Navigation.NavigationStack.FirstOrDefault ();
+				if (rootPage != null) {
+					App.IsUserLoggedIn = true;
+					Navigation.InsertPageBefore (new MainPage (), Navigation.NavigationStack.First ());
+					await Navigation.PopToRootAsync ();
+				}
+			} else {
+				messageLabel.Text = "Sign up failed";
+			}
+		}
+
+		bool AreDetailsValid (User user)
+		{
+			return (!string.IsNullOrWhiteSpace (user.FirstName) !string.IsNullOrWhiteSpace (user.LastName) !string.IsNullOrWhiteSpace (user.Username) && !string.IsNullOrWhiteSpace (user.Password) && !string.IsNullOrWhiteSpace (user.Email) && user.Email.Contains ("@"));
+		}	
+			
 	}
 }
