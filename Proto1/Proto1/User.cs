256b7570bb74709d209bf1ceeaf76ee9c7744f2e
Jason Olano <44100981+olano002@users.noreply.github.com>
2018-11-06 19:14:35 -0800

    Create User.cs

---
+++ Proto1/Proto1/User.cs
@@ -0,0 +1,17 @@
+namespace Proto1
+{
+	public class User
+	{
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+  
+		public string Username { get; set; }
+
+		public string Password { get; set; }
+    
+    public string ConfirmPassword { get; set; }
+   
+		public string Email { get; set; }
+	}
+}
