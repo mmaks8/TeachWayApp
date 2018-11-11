76e371354f8f3e19d9b5408b755a5ff397917990
Jason Olano <44100981+olano002@users.noreply.github.com>
2018-11-11 12:26:11 -0800

    Update SignUpViewModel.cs

--- Proto2/Proto2/Proto2/ViewModels/SignUpViewModel.cs
+++ Proto2/Proto2/Proto2/ViewModels/SignUpViewModel.cs
@@ -8,12 +8,46 @@ namespace Proto2.ViewModels
 {
 	public class SignUpViewModel : BindableBase
 	{
-       		private string _firstname;
+       		private string _firstName;
+		private string _lastName;
+		private string _userName;
+		private string _email;
+		private string _password;
+		private string _confirmpassword;
 		
 		public string FirstName
 		{
-			get { return _firstname;}
+			get { return _firstName;}
 			set {SetProperty(ref _firstname, value);}
+		}
+		
+		public string LastName
+		{
+			get { return _lastName;}
+			set {SetProperty(ref _lastname, value);}
+		}
+		
+		public string Email
+		{
+			get { return _email;}
+			set {SetProperty(ref _email, value);}
+		}
+		
+		public string Password
+		{
+			get { return _password;}
+			set {SetProperty(ref _password, value);}
+		}
+		
+		public string ConfirmPassword
+		{
+			get { return _confirmpassword;}
+			set {SetProperty(ref _confirmpassword, value);}
+		}
+		
+		public SignUpViewModel()
+		{
+		
 		}
 	}
 }
