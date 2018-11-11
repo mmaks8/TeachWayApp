3b93533bc305cca253db6d00b3e54e8351afef99
Jason Olano <44100981+olano002@users.noreply.github.com>
2018-11-11 13:55:58 -0800

    Update SignUpViewModel.cs

--- Proto2/Proto2/Proto2/ViewModels/SignUpViewModel.cs
+++ Proto2/Proto2/Proto2/ViewModels/SignUpViewModel.cs
@@ -8,46 +8,6 @@ namespace Proto2.ViewModels
 {
 	public class SignUpViewModel : BindableBase
 	{
-<<<<<<< HEAD
-<<<<<<< HEAD
-        private string _firstName;
-        private string _lastName;
-        private string _username;
-        private string _password;
-        private string _email;
-
-        public string FirstName
-        {
-            get { return _firstName; }
-            set { SetProperty(ref _firstName, value); }
-        }
-        public string LastName
-        {
-            get { return _lastName; }
-            set { SetProperty(ref _lastName, value); }
-        }
-        public string UserName
-        {
-            get { return _username; }
-            set { SetProperty(ref _username, value); }
-        }
-        public string Email
-        {
-            get { return _email; }
-            set { SetProperty(ref _email, value); }
-        }
-        public string Password
-        {
-            get { return _password; }
-            set { SetProperty(ref _password, value); }
-        }
-        public SignUpViewModel()
-        {
-            
-        }
-=======
-=======
->>>>>>> 9c583f574cc3a96ac71e7a2c83020bfa4840699a
        		private string _firstName;
 		private string _lastName;
 		private string _userName;
@@ -87,30 +47,12 @@ namespace Proto2.ViewModels
 		
 		partial void CheckValid ()
 		{
-			if(FirstName != null)
+			if(FirstName != null && LastName != null && UserName != null && Email != null && Password != null && ConfirmPassword != null)
 				{
-					OnSignUpButtonClicked = null;	
+					OnSignUpButtonClicked = null;
+					messageLabel = "Sign up success";
 				}
-			if(LastName != null)
-				{
-					OnSignUpButtonClicked = null;	
-				}
-			if(UserName != null)
-				{
-					OnSignUpButtonClicked = null;	
-				}
-			if(Email != null)
-				{
-					OnSignUpButtonClicked = null;	
-				}
-			if(Password != null)
-				{
-					OnSignUpButtonClicked = null;	
-				}
-			if(ConfirmPassword != null)
-				{
-					OnSignUpButtonClicked = null;	
-				}
+			
 			else
 			{
 				messageLabel = "Invalid information given / Missing Information";
@@ -121,9 +63,6 @@ namespace Proto2.ViewModels
 		{
 		
 		}
-<<<<<<< HEAD
->>>>>>> 40275e700d57bdb316b1ce8312312cc77ca1c976
-=======
->>>>>>> 9c583f574cc3a96ac71e7a2c83020bfa4840699a
+
 	}
 }
