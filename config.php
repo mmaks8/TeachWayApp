46d601cb2e135d92997ab958d05cc598690a5003
Eddy13 <32879858+Eddy13@users.noreply.github.com>
2018-12-06 09:20:33 -0800

    test

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'csusm.c0uo1rgt9ctn.us-west-2.rds.amazonaws.com');
-define('DB_USER', 'cs441');
-define('DB_PASS', 'csusmcs441');
-define('DB_DATABASE', 'TeacherWay');
-define('DB_PORT', '3306');
-?>
